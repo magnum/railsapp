ae0600914350d7115ba3400a090e6d7c32d8659a
Antonio Molinari <antoniomolinari@me.com>
2026-03-20 15:01:27 +0100

    Refactor README and update database configurations for Solid Cache and Queue. Added new schemas for cache and queue, and modified development environment settings to utilize Solid Cache and Queue adapters.

--- README.md
+++ README.md
@@ -1,24 +1,10 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+this is meant to be a rails app scaffold
 
-Things you may want to cover:
+## setup
 
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+```
+bin/rails db:create
+bin/rails db:prepare
+```
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,17 @@ Rails.application.configure do
   end
 
   # Change to :null_store to avoid any caching.
-  config.cache_store = :memory_store
+  # config.cache_store = :memory_store
+
+  # Solid Cache
+  config.cache_store = :solid_cache_store
+  config.solid_cache.connects_to = { database: { writing: :cache } }
+
+  # Solid Queue
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :queue } }
+
+
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
